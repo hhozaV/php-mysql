e52dce1b3e5e6d8ac715532c2317f90a5c676596
Vazohh <vazohhbtw@gmail.com>
2023-09-11 14:45:16 +0200

    Projet fil rouge V2

--- footer.php
+++ footer.php
@@ -1,3 +1,27 @@
-<footer id="pied_de_page">
-        <p>Copyright moi, tous droits réservés</p>
-</footer>
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <title>Site de recettes</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+        <div class="container">
+
+            <a class="navbar-brand" href="index.php">Copyright Martin Boudras, tous droits réservés</a>
+
+            </button>
+
+        </div>
+
+    </nav>
--- header.php
+++ header.php
@@ -1,10 +1,51 @@
-<nav id="menu">
-<div class="element_menu">
-<h3>Titre menu</h3>
-<ul>
-<li><a href="page1.html">Lien</a></li>
-<li><a href="page2.html">Lien</a></li>
-<li><a href="page3.html">Lien</a></li>
-</ul>
-</div>
-</nav>
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <title>Site de recettes</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+        <div class="container">
+
+            <a class="navbar-brand" href="index.php">Site de recettes</a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+
+                <span class="navbar-toggler-icon"></span>
+
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+
+                <ul class="navbar-nav ml-auto">
+
+                    <li class="nav-item">
+
+                        <a class="nav-link" href="index.php">Accueil</a>
+
+                    </li>
+
+                    <li class="nav-item">
+
+                        <a class="nav-link" href="contact.php">Contact</a>
+
+                    </li>
+
+                </ul>
+
+            </div>
+
+        </div>
+
+    </nav>
