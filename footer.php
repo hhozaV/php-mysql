11331d054019fb17c6cf2ffde049d5aa30b574e8
Vazohh <vazohhbtw@gmail.com>
2023-09-12 16:11:40 +0200

    Traitez l'envoie en PHP

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 <!-- footer.php -->
 <footer class="bg-light text-center text-lg-start mt-auto">
   <div class="text-center p-3">
-    © 2021 Copyright:
+    © 2021 Copyright :
     <a class="text-dark">Martin Boudras</a>
   </div>
 </footer>
