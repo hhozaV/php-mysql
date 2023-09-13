7e6e6b9b8fd5a037b03dde0e9e2415d546e74b83
Vazohh <vazohhbtw@gmail.com>
2023-09-13 10:27:01 +0200

    Ajout d'une clé password dans le tableau  de variables.php

--- variables.php
+++ variables.php
@@ -6,16 +6,19 @@ $users = [
         'full_name' => 'Mickaël Andrieu',
         'email' => 'mickael.andrieu@exemple.com',
         'age' => 34,
+        'password' => 'mickael'
     ],
     [
         'full_name' => 'Mathieu Nebra',
         'email' => 'mathieu.nebra@exemple.com',
         'age' => 34,
+        'password' => 'mathieu'
     ],
     [
         'full_name' => 'Laurène Castor',
         'email' => 'laurene.castor@exemple.com',
         'age' => 28,
+        'password' => 'laurene'
     ],
 ];
 
